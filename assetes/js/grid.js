fd862beaf5009026522d36896aabad8b18c747e3
reza sharifi <32232100+mshgold@users.noreply.github.com>
2022-07-01 10:34:36 +0430

    level1

---
+++ assetes/js/grid.js
@@ -0,0 +1,15 @@
+
+
+
+var humber = document.getElementById("icon-humber");
+function showmenu() {
+   
+   var mhumber= document.getElementById("menu-humber");
+    if (mhumber.className === "menu-humber d-none"){
+      
+        mhumber.className+="d-show"
+    }else{
+        mhumber.className="menu-humber d-none"
+    }
+}
+
